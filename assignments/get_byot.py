dd6f54c89164d041fbc141a60b749f91c84fc18e
Cody Kingham <codyakingham@gmail.com>
2020-04-02 11:54:14 +0100

    Add encoding statement

--- assignments/get_byot.py
+++ assignments/get_byot.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
 try:
-    your_text = list(Path('../BYOT').glob('*.txt'))[0].read_text()
+    your_text = list(Path('../BYOT').glob('*.txt'))[0].read_text(encoding='UTF-8')
 except IndexError:
     raise Exception('.txt not found in BYOT folder! Make sure your text is saved with .txt extension!')
